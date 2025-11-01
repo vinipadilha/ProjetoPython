8d29d66262cb84941778ff0fb1bf4618a30f6888
Vinicius <viniciusdarosapadilha@gmail.com>
2025-11-01 16:22:31 -0300

    feat: add login and logout routes

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,6 +12,8 @@ urlpatterns = [
     path('recursos/', include('apps.resourcesapp.urls.web')),
     path('matriculas/', include('apps.enrollments.urls.web')),
     path('aluno/', include('apps.accounts.urls.web')),
-    path('api/', include('apps.accounts.urls.api'))
+    path('api/', include('apps.accounts.urls.api')),
+    path('login/',  auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 
 ]
