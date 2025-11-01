e65e2f641c7ac2841b5f440e9636b9addb95ac39
Vinicius <viniciusdarosapadilha@gmail.com>
2025-11-01 16:27:40 -0300

    chore: configure auth redirects

--- project/settings.py
+++ project/settings.py
@@ -92,3 +92,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = '/aluno/turmas'
+LOGOUT_REDIRECT_URL = 'login'
+
+
+
