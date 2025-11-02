49bbac0ba755796fa3dadee298bbe927026ede42
Vinicius <viniciusdarosapadilha@gmail.com>
2025-11-02 13:22:44 -0300

    feat: add access rules service

--- apps/resourcesapp/services/access_rules.py
+++ apps/resourcesapp/services/access_rules.py
@@ -1,14 +1,14 @@
 from django.utils import timezone
 
-def recursos_visiveis(turma, queryset_recursos):
+def recursos_visiveis(turma, recursos_iterable):
     agora = timezone.now()
     visiveis = []
     if turma.tru_data_inicio > agora:
-        for r in queryset_recursos:
+        for r in recursos_iterable:
             if getattr(r, 'rec_acesso_previo', False):
                 visiveis.append(r)
     else:
-        for r in queryset_recursos:
+        for r in recursos_iterable:
             if not getattr(r, 'rec_draft', False):
                 visiveis.append(r)
     return visiveis
