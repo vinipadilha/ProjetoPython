e76ee946648dd7adf9699145a2a8f6202feacd59
Vinicius <viniciusdarosapadilha@gmail.com>
2025-11-02 13:29:43 -0300

    fix: corrigir posicionamento do footer na parte inferior da pagina

--- apps/accounts/views/web.py
+++ apps/accounts/views/web.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from apps.trainings.models import Turma
 from apps.resourcesapp.models import Recurso
 from apps.resourcesapp.services.access_rules import recursos_visiveis
+from apps.accounts.models import Aluno
 
 def turmas(request):
     aluno_id = 1
+    aluno = get_object_or_404(Aluno, pk=aluno_id)
     turmas = Turma.objects.filter(matriculas__aluno_id=aluno_id)
-    return render(request, 'aluno/turmas.html', {'turmas': turmas})
+    return render(request, 'aluno/turmas.html', {'turmas': turmas, 'aluno': aluno})
 
 def turma_recursos(request, turma_id):
     turma = get_object_or_404(Turma, pk=turma_id)
